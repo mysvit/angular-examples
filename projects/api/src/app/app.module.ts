e51c4c7900a7e4a3d5533c688087c540b6423c4a
Summer Mix <smix@tuta.io>
2021-12-09 17:04:56 -0500

    function equilibristics fix

--- projects/api/src/app/app.module.ts
+++ projects/api/src/app/app.module.ts
@@ -7,14 +7,13 @@ import {HttpErrorHandler} from './http/http-error-handler.service'
 import {httpInterceptorProviders} from './http-interceptors'
 import {AuthService} from './http/auth.service'
 import {MatButtonModule} from '@angular/material/button'
-import {FormsModule} from '@angular/forms'
 
 @NgModule({
     declarations: [
         AppComponent
     ],
     imports: [
-        BrowserModule, HttpClientModule, MatButtonModule, FormsModule
+        BrowserModule, HttpClientModule, MatButtonModule
     ],
     providers: [
         HttpLogService,
