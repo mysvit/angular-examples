1d04db939beeed4596c354fa5c74079e422cbfc4
Summer Mix <smix@tuta.io>
2021-12-09 17:35:19 -0500

    FE test

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Angular Examples
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.0.1.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.0.4.
 
 # angular-examples
 Created from empty multiple projects:
--- projects/function-equilibristics/src/app/test/test.component.ts
+++ projects/function-equilibristics/src/app/test/test.component.ts
@@ -6,4 +6,25 @@ import {Component} from '@angular/core';
     styleUrls: ['./test.component.scss']
 })
 export class TestComponent {
+
+    testClick() {
+        console.log(solveMeFirst(1, 5))
+        console.log(compareTriplets([1, 2, 3], [2, 2, 5]))
+    }
+
+}
+
+// @ts-ignore
+function solveMeFirst(a, b) {
+    return ((a >= 1 && a <= 1000) ? a : 0) + ((b >= 1 && b <= 1000) ? b : 0)
+}
+
+// @ts-ignore
+function compareTriplets(a, b) {
+    let res = [0, 0]
+    for (let i = 0; i < 3; i++) {
+        res[0] += a[i] > b[i] ? 1 : 0
+        res[1] += a[i] < b[i] ? 1 : 0
+    }
+    return res
 }
