dcbbea4f13e4d19abbe7d4403b5aea958511075e
Summer Mix <smix@tuta.io>
2021-12-12 16:17:00 -0500

    move codingame & hackerrank

--- projects/function-equilibristics/src/app/app.module.ts
+++ projects/function-equilibristics/src/app/app.module.ts
@@ -3,15 +3,11 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {ArrayComponent} from './array/array.component';
 import {FormsModule} from '@angular/forms';
-import {HackerRankComponent} from './hacker-rank/hacker-rank.component';
-import { CodeGameComponent } from './code-game/code-game.component'
 
 @NgModule({
     declarations: [
         AppComponent,
         ArrayComponent,
-        HackerRankComponent,
-        CodeGameComponent
     ],
     imports: [
         BrowserModule,
