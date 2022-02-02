b1720827f5c7a90c21d65c56693a2b30dd2942d3
Duck <sergii@empower.social>
2022-02-02 14:31:25 -0500

    flex

--- projects/style-scss/src/app/app.module.ts
+++ projects/style-scss/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ImgComponent } from './style/img/img.component';
+import { FlexComponent } from './style/flex/flex.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImgComponent
+    ImgComponent,
+    FlexComponent
   ],
   imports: [
     BrowserModule
