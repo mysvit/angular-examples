57172a72439b881449cd1f93a196e080241d995b
Summer Mix <smix@tuta.io>
2022-02-16 09:26:06 -0500

    table tag - scroll body

--- projects/style-scss/src/app/app.module.ts
+++ projects/style-scss/src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { ImgComponent } from './style/img/img.component';
-import { FlexComponent } from './style/flex/flex.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {ImgComponent} from './style/img/img.component';
+import {FlexComponent} from './style/flex/flex.component';
+import {TagTableComponent} from './style/tag-table/tag-table.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ImgComponent,
-    FlexComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ImgComponent,
+        FlexComponent,
+        TagTableComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
