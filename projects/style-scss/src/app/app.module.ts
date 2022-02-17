1a1bd2c4a7974156beaff7341af5ac3189e8edf6
Summer Mix <smix@tuta.io>
2022-02-17 12:58:38 -0500

    Disable div

--- projects/style-scss/src/app/app.module.ts
+++ projects/style-scss/src/app/app.module.ts
@@ -4,13 +4,15 @@ import {AppComponent} from './app.component';
 import {ImgComponent} from './style/img/img.component';
 import {FlexComponent} from './style/flex/flex.component';
 import {TagTableComponent} from './style/tag-table/tag-table.component';
+import { TagDivComponent } from './style/tag-div/tag-div.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         ImgComponent,
         FlexComponent,
-        TagTableComponent
+        TagTableComponent,
+        TagDivComponent
     ],
     imports: [
         BrowserModule
---
+++ projects/style-scss/src/app/style/tag-div/tag-div.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-tag-div',
+    templateUrl: './tag-div.component.html',
+    styleUrls: ['./tag-div.component.scss']
+})
+export class TagDivComponent implements OnInit {
+
+    click = 0
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+    clickCommand() {
+        this.click++;
+    }
+}
